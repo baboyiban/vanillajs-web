1d42af596f8a0b946ea5756f3c2a87c367e357dc
baboyiban <chl11wq12@gmail.com>
2025-05-13 16:44:28 +0900

    init

---
+++ backend/build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // 모듈 생성 + target / optimize 포함
+    const exe_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // 실행 파일 정의 → target / optimize 생략!
+    const exe = b.addExecutable(.{
+        .name = "backend",
+        .root_module = exe_module,
+    });
+
+    // 설치 및 실행 설정
+    b.installArtifact(exe);
+
+    const zap = b.dependency("zap", .{
+        .target = target,
+        .optimize = optimize,
+        .openssl = false, // set to true to enable TLS support
+    });
+
+    exe.root_module.addImport("zap", zap.module("zap"));
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
